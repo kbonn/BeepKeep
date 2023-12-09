35b7e71435438e307c59e96f26e91735bd0eca8e
KevinBonnett <kevin.bonnett@voyagesms.com>
2023-12-08 22:20:34 -0700

    added color shapes

--- app/src/main/java/com/example/beepkeep/MainActivity.kt
+++ app/src/main/java/com/example/beepkeep/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.beepkeep
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,6 +24,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,6 +32,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,6 +62,10 @@ fun BeepKeepApp(modifier: Modifier = Modifier) {
 
 @Composable
 fun BPEntryCard() {
+    var systolicColor: Color by remember { mutableStateOf(Color.Gray) }
+    var diastolicColor: Color by remember { mutableStateOf(Color.Gray) }
+    var pulseColor: Color by remember { mutableStateOf(Color.Gray) }
+
     Column {
         Spacer(modifier = Modifier.weight(1f))
         Card(
@@ -68,9 +80,9 @@ fun BPEntryCard() {
                     .padding(16.dp)
                     .fillMaxSize()
             ) {
-                DataField(label = "Systolic")
-                DataField(label = "Diastolic")
-                DataField(label = "Pulse")
+                DataField(label = "Systolic", systolicColor)
+                DataField(label = "Diastolic", diastolicColor)
+                DataField(label = "Pulse", pulseColor)
                 Button(
                     onClick = { /*TODO*/ },
                     modifier = Modifier
@@ -90,15 +102,26 @@ fun BPEntryCard() {
 @Composable
 fun DataField(
     label: String,
+    color: Color
     ) {
     var text by remember { mutableStateOf("") }
 
-    OutlinedTextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text(label) },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-    )
+    Row {
+        Box(
+            modifier = Modifier
+                .size(20.dp)
+                .clip(shape)
+                .background(color)
+                .align(Alignment.CenterVertically)
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(label) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+    }
 }
 
 @Preview
